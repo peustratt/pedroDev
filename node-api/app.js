6f6b189c2e981ab036d616b6da66b05f6a7be42a
peustratt <peustratt@gmail.com>
2022-09-21 18:10:17 -0300

    --no-edit

--- node-api/app.js
+++ node-api/app.js
@@ -4,6 +4,8 @@ const helmet = require("helmet");
 const logger = require("morgan");
 const createError = require("http-errors");
 
+const { Project } = require("./routes");
+
 const app = express();
 
 // config middleware
@@ -14,6 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // config routes
+app.use("/api/project", Project);
+
 app.use((req, res, next) => {
   next(createError(404));
 });
--- node-api/routes/project.js
+++ node-api/routes/project.js
@@ -1,12 +1,10 @@
 const router = require("express").Router();
 
-router
-  .route("/")
-  .get((req, res, next) =>
-    Promise.resolve()
-      .then(() => ({ name: "project" }))
-      .then((project) => res.json(project))
-  )
-  .catch((err) => next(err));
+router.route("/").get((req, res, next) =>
+  Promise.resolve()
+    .then(() => ({ name: "project" }))
+    .then((project) => res.json(project))
+    .catch((err) => next(err))
+);
 
 module.exports = router;
