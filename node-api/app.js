ea7d8fd4e4ff157d13e15887593b52abce7fd149
peustratt <peustratt@gmail.com>
2022-09-21 18:07:38 -0300

    project setup

---
+++ node-api/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const logger = require("morgan");
+const createError = require("http-errors");
+
+const app = express();
+
+// config middleware
+app.use(cors());
+app.use(helmet());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// config routes
+app.use((req, res, next) => {
+  next(createError(404));
+});
+// error handling middleware
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ message: "Something went wrong" });
+});
+
+module.exports = app;
---
+++ node-api/routes/project.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+
+router
+  .route("/")
+  .get((req, res, next) =>
+    Promise.resolve()
+      .then(() => ({ name: "project" }))
+      .then((project) => res.json(project))
+  )
+  .catch((err) => next(err));
+
+module.exports = router;
